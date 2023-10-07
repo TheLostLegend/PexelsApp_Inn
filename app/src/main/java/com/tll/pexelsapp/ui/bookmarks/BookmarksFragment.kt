28aaa3977bcb9662f2164f905a89305b74edab91
TheLostLegend <arhir.13.07.2002@gmail.com>
2023-10-07 23:34:04 +0300

    Almost working base

--- app/src/main/java/com/tll/pexelsapp/ui/bookmarks/BookmarksFragment.kt
+++ app/src/main/java/com/tll/pexelsapp/ui/bookmarks/BookmarksFragment.kt
@@ -1,4 +1,4 @@
-package com.tll.pexelsapp.ui.dashboard
+package com.tll.pexelsapp.ui.bookmarks
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.tll.pexelsapp.databinding.FragmentDashboardBinding
+import com.tll.pexelsapp.databinding.FragmentBookmarksBinding
 
-class DashboardFragment : Fragment() {
+class BookmarksFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding?=null
+    private var _binding: FragmentBookmarksBinding?=null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup? ,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel=
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val bookmarksViewModel=
+            ViewModelProvider(this)[BookmarksViewModel::class.java]
 
-        _binding=FragmentDashboardBinding.inflate(inflater , container , false)
+        _binding = FragmentBookmarksBinding.inflate(inflater , container , false)
         val root: View=binding.root
 
-        val textView: TextView=binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView=binding.textBookmarks
+        bookmarksViewModel.text.observe(viewLifecycleOwner) {
             textView.text=it
         }
         return root
--- app/src/main/java/com/tll/pexelsapp/ui/bookmarks/BookmarksViewModel.kt
+++ app/src/main/java/com/tll/pexelsapp/ui/bookmarks/BookmarksViewModel.kt
@@ -1,13 +1,13 @@
-package com.tll.pexelsapp.ui.notifications
+package com.tll.pexelsapp.ui.bookmarks
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class BookmarksViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value="This is notifications Fragment"
+        value="This is bookmarks Fragment"
     }
     val text: LiveData<String> = _text
 }
